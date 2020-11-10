473d0f05fca707e71636065b442d331c8d5167ae
Emilio Wuerges <wuerges@gmail.com>
2020-11-10 09:57:49 -0300

    added schema

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+extern crate diesel;
+
+use self::diesel::prelude::*;
+
+use turbinator::models::*;
+use turbinator::*;
+// use turbinator::models::*;
+
 fn main() {
-    println!("Hello, world!");
+
+    let connection = establish_connection();
+    let results = posts.filter(published.eq(true))
+        .limit(5)
+        .load::<Post>(&connection)
+        .expect("Error loading posts");
+
+    println!("Displaying {} posts", results.len());
+    for post in results {
+        println!("{}", post.title);
+        println!("----------\n");
+        println!("{}", post.body);
+    }
 }
