957f6cbb8840bfd0c9cdf042acbc7edde7a96b2e
Emilio Wuerges <wuerges@gmail.com>
2020-11-10 10:59:49 -0300

    generated database model for diesel

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,28 @@ extern crate diesel;
 use self::diesel::prelude::*;
 
 use turbinator::models::*;
+use turbinator::schema::*;
 use turbinator::*;
 // use turbinator::models::*;
 
 fn main() {
 
-    let connection = establish_connection();
-    let results = posts.filter(published.eq(true))
-        .limit(5)
-        .load::<Post>(&connection)
-        .expect("Error loading posts");
+    use runtable::dsl;
 
-    println!("Displaying {} posts", results.len());
-    for post in results {
-        println!("{}", post.title);
+    let connection = establish_connection();
+
+    let runs : i64 = dsl::runtable.count()
+        .get_result(&connection)
+        .expect("Error counting runs");
+
+    println!("Displaying {:?} runs", runs);
+    let results = dsl::runtable
+        .limit(5)
+        .load::<Runtable>(&connection)
+        .expect("Error loading runs");
+    for run in results {
+        println!("{}", run.usernumber);
         println!("----------\n");
-        println!("{}", post.body);
+        println!("{}", run.runproblem);
     }
 }
